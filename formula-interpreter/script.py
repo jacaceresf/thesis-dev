ed6177da3e5cf1e970bc38c88de134e1eef107f3
Jorge Caceres <jorge.caceres@bfgo.mx>
2022-01-13 21:56:03 -0300

    Update script. Fix interpreter notebook

--- formula-interpreter/script.py
+++ formula-interpreter/script.py
@@ -1,28 +1,42 @@
+import pandas as pd
+import numpy as np
+from patsy import ModelDesc, dmatrices, dmatrix, demo_data
+import re
+import pprint
+import json
+
+COMPLEX_OPERATIONS = {
+    'cos': 'np.cos',
+    'tan': 'np.tan',
+    'log': 'np.log'
+}
+
+def add_blank_spaces_to_formula(formula: str) -> str:
+    new = ''
+    for index, element in enumerate(formula):
+        next_idx = index + 1
+        if next_idx < len(formula):
+            if not re.match('\w', formula[index+1]):
+                new += element + ' '
+            else:
+                new += element
+        else:
+            new += element + ' '
+    return new
+
+def clean_formula(formula: str) -> str:
+    result = formula
+    for operation in COMPLEX_OPERATIONS:
+        if(operation in formula):
+            result = formula.replace(operation, "")
+    return result
+
 def get_formula_variables(formula: str):
   '''
   Returns a list of every variable (non repeated) from the formula
   '''
-  return list(set("".join(re.findall("[a-zA-Z]+", formula))))
-
-def get_formula_by_columns(formula: str, columns: list) -> dict:
-  '''
-  Mapping every single formula's variable to a column.
-  '''
-  to_replace = {}
-
-  # formula variables
-  variables = get_formula_variables(formula=formula)
-
-  # iterate over grouped columns
-  for cidx, column_group in enumerate(columns):
-    formula_grouped = {}
-    # iterate over variables
-    for idx, variable in enumerate(variables):
-      # variable paired to column name
-      formula_grouped[variable] = column_group[idx]
-    # every column group represents a key
-    to_replace[cidx] = formula_grouped
-  return to_replace
+  cleaned_formula = clean_formula(formula)
+  return sorted(list(set("".join(re.findall("[a-zA-Z]+", cleaned_formula)))))
 
 def group_columns(formula: str, data: pd.DataFrame):
   # get number of variables inside formula
@@ -38,14 +52,33 @@ def group_columns(formula: str, data: pd.DataFrame):
   
   # column by column
   for i in range(0, columns):  
-    # current column + 1
-    for j in range(i+1, columns, characters_len):
-      lst = [columns_lst[i]]
-      lst.extend(columns_lst[j:j+(characters_len-1)])
+    # current column + 1 and substract 1 from characters so we don't count current character
+    for j in range(i+1, columns, characters_len-1):
+      column_variables = [columns_lst[i]]
+      column_variables.extend(columns_lst[j:j+(characters_len-1)])
       # compare numbers and group columns by number of variables inside the formula
-      if(len(lst) == characters_len):
-        result.append(lst)
+      if(len(column_variables) == characters_len):
+        result.append(column_variables)
   return result # grouped columns
+
+def get_formula_by_columns(formula: str, columns: list) -> dict:
+  '''
+  Mapping every single formula's variable to a column.
+  '''
+  to_replace = {}
+
+  # formula variables
+  variables = get_formula_variables(formula=formula)
+  # iterate over grouped columns
+  for cidx, column_group in enumerate(columns):
+    formula_grouped = {}
+    # iterate over variables
+    for idx, variable in enumerate(variables):
+      # variable paired to column name
+      formula_grouped[variable] = column_group[idx]
+    # every column group represents a key
+    to_replace[cidx] = formula_grouped
+  return to_replace
 
 def parse_formula(formula: str, formula_columns: dict) -> list:
   '''
@@ -53,15 +86,21 @@ def parse_formula(formula: str, formula_columns: dict) -> list:
   In simple words, replaces every formula variable for its paired column.
   '''
   result = []
+  formula_variables = re.findall(r'\w+', formula)
 
-  for key, columns in formula_columns.items():
-    new_formula = ""
-    for element in formula:
-      if element in columns:
-        new_formula += columns[element]
-      else:
-        new_formula += element
-    result.append(new_formula)
+  for variables_paired in formula_columns.values():
+        new_formula = formula
+        for variable in formula_variables:
+            if variable in variables_paired:
+                # we need to put a blank space after a single character, 
+                # so we can identify it then with the regex
+                replace_regex = f'{variable}(?:[^\w\*\\\+\(\)])'
+                new_formula = re.sub(replace_regex, variables_paired[variable], new_formula)
+            elif variable in COMPLEX_OPERATIONS:
+                new_formula = new_formula.replace(variable, COMPLEX_OPERATIONS[variable])
+        new_formula = new_formula.replace(" ", "")
+
+        result.append(new_formula)
   
   return result
 
@@ -71,25 +110,27 @@ def execute_formula(formula_by_columns: list, data: pd.DataFrame) -> pd.DataFram
   Saves every formula result inside a new dataframe's column.
   '''
   new_df = data.copy()
-  for formula in formula_by_columns:
-    f = "I("+formula+")-1"
-    print(f'Formula: [{f}]')
-    result = dmatrix(f, data)
+  for formula_columns in formula_by_columns:
+    formula = "I("+formula_columns+")-1"
+    result = dmatrix(formula, data)
     result_items = []
     for item in result:
       result_items.append(item.item())
-    new_df[formula] = result_items
+    
+    new_df[formula_columns.replace('np.', '')] = result_items
 
   return new_df
-  
-formula = '(a * a) / c'
 
+def execute(formula_input: str, data: pd.DataFrame) -> pd.DataFrame:
+    
+    formula = add_blank_spaces_to_formula(formula_input)
+    
+    grouped_columns = group_columns(formula, data)
 
-grouped_columns = group_columns(formula, x)
+    replaceable_result = get_formula_by_columns(formula, grouped_columns)
 
-replaceable_result = get_formula_by_columns(formula, grouped_columns)
+    executable_formulas = parse_formula(formula, replaceable_result)
 
-executable_formulas = parse_formula(formula, replaceable_result)
-
-new_data = execute_formula(executable_formulas, x)
-new_data.head(3)
+    new_data = execute_formula(executable_formulas, data)
+    
+    return new_data
